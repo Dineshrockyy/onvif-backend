82063cdab6883168cf146aff3799847746963887
SIEORA <dineshbabu6369@gmail.com>
2023-10-30 12:35:04 +0530

    second commit

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
-var onvif = require('onvif');
-onvif.Discovery.on('device', function (cam) {
-    share(cam)
-    // console.log(cam);
-    // cam.connect();
+const express = require('express');
+const cors = require('cors');
+const onvif = require('onvif');
+const app = express();
+const PORT = 5000;
 
-})
-// Must have an error handler to catch bad replies from the network
-onvif.Discovery.on('error', function (err, xml) {
-    // function called as soon as NVT responds, but this library could not parse the response
-    console.log('Discovery error ' + err);
-});
-onvif.Discovery.probe();
+app.use(cors());
+app.use(express.json());
 
-function share(ip) {
-    new onvif.Cam({
-        hostname: ip.hostname,
-        username: 'admin',
-        password: 'Sieora123'
-    }, function (err) {
-        try {
-            this.getStreamUri({ protocol: 'RTSP' }, function (err, stream) {
-                console.log(stream);
-            });
-        } catch {
-            console.log(ip.hostname);
-        }
+app.get('/discover', (req, res) => {
+    const devices = [];
+    onvif.Discovery.on('device', function (cam) {
+        share(cam)
+        // console.log(cam);
+        // cam.connect();
+
+    })
+    // Must have an error handler to catch bad replies from the network
+    onvif.Discovery.on('error', function (err, xml) {
+        // function called as soon as NVT responds, but this library could not parse the response
+        console.log('Discovery error ' + err);
     });
-}
+    onvif.Discovery.probe();
 
-// var
-//     http = require('http'),
-//     Cam = require('onvif').Cam;
+    function share(ip) {
+        new onvif.Cam({
+            hostname: ip.hostname,
+            username: 'admin',
+            password: 'Sieora123'
+        }, function (err) {
+            try {
+                this.getStreamUri({ protocol: 'RTSP' }, function (err, stream) {
+                    devices.push(stream)
+                    res.json(devices)
+                    console.log(stream);
+                });
+            } catch {
+                console.log(ip.hostname);
+            }
+        });
+    }
+    res.json(devices)
 
-// new Cam({
-//     hostname: '192.168.1.62',
-//     username: 'admin',
-//     password: 'Sieora123'
-// }, function (err) {
-//     this.getStreamUri({ protocol: 'RTSP' }, function (err, stream) {
-//         console.log(stream);
-//     });
-// });
+});
 
-// setInterval(function () {
-//     var onvif = require('node-onvif');
-//     onvif.startDiscovery(function (info) {
-//         console.log(info);
-//         setTimeout(function () {
-//             console.log('stop');
-//             onvif.stopDiscovery();
-//         }, 3000);
-//     });
-// }, 5000)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
