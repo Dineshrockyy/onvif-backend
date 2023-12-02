12c848aa8c7fb4e9979d3ecb6b0ee748289cf99f
SIEORA <dineshbabu6369@gmail.com>
2023-12-02 11:17:09 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 const express = require('express');
 const cors = require('cors');
-const onvif = require('onvif');
+const Stream = require("node-rtsp-stream")
 const app = express();
 const PORT = 5000;
+let stream = null
 
 app.use(cors());
 app.use(express.json());
 
+// app.post("/stream", (req, res) => {
+//     const newRtspStreamUrl = req.body.rtsp
+//     let currentRtspStreamUrl = ""
+//     console.log(newRtspStreamUrl);
 
-app.get('/discover', (req, res) => {
-    let address = req.body.ips
-    const devices = [];
-    let count = 0
-    address.map((val) => {
-        new onvif.Cam({
-            hostname: val,
-            username: req.body.username,
-            password: req.body.password
-        }, function (err) {
-            try {
-                this.getStreamUri({ protocol: 'RTSP' }, function (err, stream) {
-                    count = count + 1
-                    devices.push(stream)
-                    console.log(stream);
-                    if (address.length === count) {
-                        res.json(devices)
-                    }
-                });
+//     // Create the WebSocket stream only if it doesn't exist or the RTSP URL has changed
+//     if (!stream || currentRtspStreamUrl !== newRtspStreamUrl) {
+//         if (stream || newRtspStreamUrl === "stop") {
+//             stream.stop()
+//         }
 
-                // this.getDeviceInformation(function (err, info, xml) {
-                //     console.log(info);
-                // })
-            } catch {
-                count = count + 1
-                console.log('error',val);
-                if (address.length === count) {
-                    res.json(devices)
-                }
-            }
-        });
+//         stream = new Stream({
+//             name: 'Camera Stream',
+//             streamUrl: newRtspStreamUrl,
+//             wsPort: 9999,
+//             ffmpegOptions: { // options ffmpeg flags
+//                 '-stats': '', // an option with no neccessary value uses a blank string
+//                 '-r': 30 // options with required values specify the value after the key
+//             }
+//         })
+//         currentRtspStreamUrl = newRtspStreamUrl
+//     }
+
+//     res.send(200).json({ url: `ws://127.0.0.1:9999` })
+// })
+
+app.post("/stream", (req, res) => {
+    stream = new Stream({
+        name: 'name',
+        streamUrl: 'rtsp://aitest:Ac72!k34j2r1@184.69.129.222:8012/streaming/channels/1002',
+        wsPort: 9999,
+        ffmpegOptions: { // options ffmpeg flags
+            '-stats': '', // an option with no neccessary value uses a blank string
+            '-r': 30 // options with required values specify the value after the key
+        }
     })
-
-});
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
